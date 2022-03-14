f43cf850a64d625d30508d39931f17ef2f1db621
David Ilie <david@davidilie.com>
2022-03-14 11:12:11 +0100

    coding

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useSession } from "next-auth/react";
+import { useSession, signIn } from "next-auth/react";
 
 import { Button } from "../../ui/Button";
 import { Spinner } from "../../ui/Spinner";
@@ -9,7 +9,7 @@ const NavBar: React.FC = () => {
     const { status } = useSession();
 
     return (
-        <header className="body-font border-b-2 text-gray-600 sm:px-8">
+        <header className="body-font border-b-2 text-gray-600 sm:px-12">
             <div className="container mx-auto flex flex-col flex-wrap items-center p-5 md:flex-row">
                 <a className="title-font mb-4 flex items-center font-medium text-gray-900 md:mb-0">
                     <svg
@@ -71,9 +71,9 @@ const NavBar: React.FC = () => {
                         </button>
                     </div>
                     {status === "loading" ? (
-                        <Spinner />
+                        <Spinner size="6" />
                     ) : status === "unauthenticated" ? (
-                        <Button>Login</Button>
+                        <Button onClick={() => signIn("google")}>Login</Button>
                     ) : (
                         <UserDropdown />
                     )}
--- src/components/NavBar/UserDropdown.tsx
+++ src/components/NavBar/UserDropdown.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import { Menu, Transition } from "@headlessui/react";
 import { Fragment } from "react";
 import { BiUserCircle, BiCog } from "react-icons/bi";
-import { MdManageAccounts } from "react-icons/md";
-import Link from "next/link";
-import { useSession } from "next-auth/react";
+import { useSession, signOut } from "next-auth/react";
 
 import DropdownElement from "../../ui/DropdownElement";
 import NextLink from "../../ui/NextLink";
@@ -12,12 +10,12 @@ import NextLink from "../../ui/NextLink";
 const UserDropdown: React.FC = () => {
     const { data } = useSession();
     return (
-        <div className="flex items-center sm:ml-6">
+        <div className="flex items-center">
             <Menu as="div" className="relative inline-block text-right">
                 <Menu.Button className="inline-flex items-center text-black">
                     <img
                         src={data?.user?.image || "/no-pfp.jpg"}
-                        className="mt-0.5 -ml-1 flex rounded-full border-[4px] border-red-500"
+                        className="flex w-1/2 rounded-full border-[2px] border-red-500"
                     />
                 </Menu.Button>
                 <Transition
@@ -31,40 +29,25 @@ const UserDropdown: React.FC = () => {
                 >
                     <Menu.Items className="absolute right-0 z-10 mt-1 w-36 rounded-md border border-gray-200 bg-gray-50 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                         <div className="border-1 rounded-t-md border-gray-300">
-                            <Menu.Item
-                                as={NextLink}
-                                href="/user/settings?tab=profile"
-                            >
+                            <Menu.Item as={NextLink} href="/profile">
                                 <DropdownElement>
                                     <BiUserCircle className="mx-0.5 text-xl" />
                                     Profile
                                 </DropdownElement>
                             </Menu.Item>
-                            <Menu.Item
-                                as={NextLink}
-                                href="/user/settings?tab=account"
-                            >
-                                <DropdownElement>
-                                    <MdManageAccounts className="mx-0.5 text-xl" />
-                                    Account
-                                </DropdownElement>
-                            </Menu.Item>
-                            <Menu.Item
-                                as={NextLink}
-                                href="/user/settings?tab=subjects"
-                            >
+                            <Menu.Item as={NextLink} href="/settings">
                                 <DropdownElement>
                                     <BiCog className="mx-0.5 text-xl" />
                                     Settings
                                 </DropdownElement>
                             </Menu.Item>
                         </div>
-                        <Menu.Item>
-                            <Link href="/logout">
-                                <a className="group flex w-full cursor-pointer items-center justify-center rounded-b-md bg-blue-600 py-2 text-center text-sm font-semibold text-white duration-150 hover:bg-blue-700">
-                                    Log Out
-                                </a>
-                            </Link>
+                        <Menu.Item
+                            as="a"
+                            className="group flex w-full cursor-pointer items-center justify-center rounded-b-md bg-blue-600 py-2 text-center text-sm font-semibold text-white duration-150 hover:bg-blue-700"
+                            onClick={() => signOut()}
+                        >
+                            Log Out
                         </Menu.Item>
                     </Menu.Items>
                 </Transition>
