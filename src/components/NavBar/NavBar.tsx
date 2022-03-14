2353c9f88335286f86f79e5ebef308bc4d923d68
David Ilie <david@davidilie.com>
2022-03-14 11:55:13 +0100

    coding

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -9,7 +9,7 @@ const NavBar: React.FC = () => {
     const { status } = useSession();
 
     return (
-        <header className="body-font border-b-2 text-gray-600 sm:px-12">
+        <nav className="body-font border-b-2 text-gray-600 sm:px-12">
             <div className="container mx-auto flex flex-col flex-wrap items-center p-5 md:flex-row">
                 <a className="title-font mb-4 flex items-center font-medium text-gray-900 md:mb-0">
                     <svg
@@ -79,7 +79,7 @@ const NavBar: React.FC = () => {
                     )}
                 </div>
             </div>
-        </header>
+        </nav>
     );
 };
 
--- src/components/NavBar/UserDropdown.tsx
+++ src/components/NavBar/UserDropdown.tsx
@@ -10,49 +10,47 @@ import NextLink from "../../ui/NextLink";
 const UserDropdown: React.FC = () => {
     const { data } = useSession();
     return (
-        <div className="flex items-center">
-            <Menu as="div" className="relative inline-block text-right">
-                <Menu.Button className="inline-flex items-center text-black">
-                    <img
-                        src={data?.user?.image || "/no-pfp.jpg"}
-                        className="flex w-1/2 rounded-full border-[2px] border-red-500"
-                    />
-                </Menu.Button>
-                <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                >
-                    <Menu.Items className="absolute right-0 z-10 mt-1 w-36 rounded-md border border-gray-200 bg-gray-50 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        <div className="border-1 rounded-t-md border-gray-300">
-                            <Menu.Item as={NextLink} href="/profile">
-                                <DropdownElement>
-                                    <BiUserCircle className="mx-0.5 text-xl" />
-                                    Profile
-                                </DropdownElement>
-                            </Menu.Item>
-                            <Menu.Item as={NextLink} href="/settings">
-                                <DropdownElement>
-                                    <BiCog className="mx-0.5 text-xl" />
-                                    Settings
-                                </DropdownElement>
-                            </Menu.Item>
-                        </div>
-                        <Menu.Item
-                            as="a"
-                            className="group flex w-full cursor-pointer items-center justify-center rounded-b-md bg-blue-600 py-2 text-center text-sm font-semibold text-white duration-150 hover:bg-blue-700"
-                            onClick={() => signOut()}
-                        >
-                            Log Out
+        <Menu as="div" className="relative -mr-14 inline-flex">
+            <Menu.Button>
+                <img
+                    src={data?.user?.image || "/no-pfp.jpg"}
+                    className="flex w-[40%] rounded-full"
+                />
+            </Menu.Button>
+            <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+            >
+                <Menu.Items className="absolute right-0 z-10 mt-12 w-36 rounded-md border border-gray-200 bg-gray-50 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <div className="border-1 rounded-t-md border-gray-300">
+                        <Menu.Item as={NextLink} href="/profile">
+                            <DropdownElement>
+                                <BiUserCircle className="mx-0.5 text-xl" />
+                                Profile
+                            </DropdownElement>
                         </Menu.Item>
-                    </Menu.Items>
-                </Transition>
-            </Menu>
-        </div>
+                        <Menu.Item as={NextLink} href="/settings">
+                            <DropdownElement>
+                                <BiCog className="mx-0.5 text-xl" />
+                                Settings
+                            </DropdownElement>
+                        </Menu.Item>
+                    </div>
+                    <Menu.Item
+                        as="a"
+                        className="group flex w-full cursor-pointer items-center justify-center rounded-b-md bg-blue-600 py-2 text-center text-sm font-semibold text-white duration-150 hover:bg-blue-700"
+                        onClick={() => signOut()}
+                    >
+                        Log Out
+                    </Menu.Item>
+                </Menu.Items>
+            </Transition>
+        </Menu>
     );
 };
 
