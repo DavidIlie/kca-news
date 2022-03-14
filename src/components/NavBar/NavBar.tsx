d03b3e89079b222817bbe89e84160fde46596f34
David Ilie <david@davidilie.com>
2022-03-14 16:07:36 +0100

    shadow

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -19,7 +19,7 @@ const NavBar: React.FC = () => {
     const { status } = useSession();
 
     return (
-        <nav className="w-full border-b-2 text-gray-600 sm:fixed sm:px-12">
+        <nav className="w-full border-b-2 text-gray-600 shadow-md sm:fixed sm:px-12">
             <div className="container mx-auto flex flex-col flex-wrap items-center p-5 md:flex-row">
                 <Link href="/">
                     <a className="mb-4 flex items-center font-medium text-gray-900 md:mb-0">
