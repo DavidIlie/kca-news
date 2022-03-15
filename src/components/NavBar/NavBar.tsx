b8fbfa2f3e3270683ba104b77500548a37f990f3
David Ilie <david@davidilie.com>
2022-03-15 12:08:52 +0100

    now is the time we create the backend

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 import { useSession, signIn } from "next-auth/react";
 import { AiOutlineSearch } from "react-icons/ai";
+import { Bounce } from "react-awesome-reveal";
 
 import { Button } from "../../ui/Button";
 import { Spinner } from "../../ui/Spinner";
@@ -19,64 +20,71 @@ const NavBar: React.FC = () => {
     const { status } = useSession();
 
     return (
-        <nav className="z-50 w-full border-b-2 bg-white text-gray-600 shadow-md sm:fixed 2xl:px-12">
-            <div className="container mx-auto flex flex-col flex-wrap items-center p-5 md:flex-row">
-                <Link href="/">
-                    <a className="mb-4 flex items-center font-medium text-gray-900 md:mb-0">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            className="h-10 w-10 rounded-full bg-blue-500 p-2 text-white"
-                            viewBox="0 0 24 24"
-                        >
-                            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                        </svg>
-                        <span className="ml-3 text-xl">KCA News</span>
-                    </a>
-                </Link>
-                <nav className="flex flex-wrap items-center justify-center gap-4 text-base md:mr-auto md:ml-4 md:border-l md:border-gray-400 md:py-1 md:pl-4">
+        <Bounce direction="down" triggerOnce>
+            <nav className="z-50 w-full border-b-2 bg-white text-gray-600 shadow-md sm:fixed 2xl:px-12">
+                <div className="container mx-auto flex flex-col flex-wrap items-center p-5 md:flex-row">
                     <Link href="/">
-                        <a className="cursor-pointer rounded-md border-2 border-gray-200 bg-gray-100 px-2 py-1 duration-150 hover:bg-gray-200 hover:text-gray-900">
-                            Home
+                        <a className="mb-4 flex items-center font-medium text-gray-900 md:mb-0">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                className="h-10 w-10 rounded-full bg-blue-500 p-2 text-white"
+                                viewBox="0 0 24 24"
+                            >
+                                <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                            </svg>
+                            <span className="ml-3 text-xl">KCA News</span>
                         </a>
                     </Link>
-                    <NewsLink name="News" categories={newsLinks} />
-                    <NewsLink
-                        name="Entertainment"
-                        categories={entertainmentLinks}
-                    />
-                    <NewsLink name="Sport" categories={sportLinks} />
-                    <NewsLink name="Lifestyle" categories={lifestyleLinks} />
-                    <Link href="/our-team">
-                        <a className="cursor-pointer rounded-md border-2 border-gray-200 bg-gray-100 px-2 py-1 duration-150 hover:bg-gray-200 hover:text-gray-900">
-                            Our Team
-                        </a>
-                    </Link>
-                </nav>
-                <div className="mx-auto mt-4 flex items-center gap-4 xl:mt-0">
-                    <div className="relative mx-auto text-gray-600">
-                        <input
-                            className="h-10 rounded-lg border-2 border-gray-300 bg-white px-5 pr-16 text-sm focus:outline-none"
-                            type="search"
-                            name="search"
-                            placeholder="Search"
+                    <nav className="flex flex-wrap items-center justify-center gap-4 text-base md:mr-auto md:ml-4 md:border-l md:border-gray-400 md:py-1 md:pl-4">
+                        <Link href="/">
+                            <a className="cursor-pointer rounded-md border-2 border-gray-200 bg-gray-100 px-2 py-1 duration-150 hover:bg-gray-200 hover:text-gray-900">
+                                Home
+                            </a>
+                        </Link>
+                        <NewsLink name="News" categories={newsLinks} />
+                        <NewsLink
+                            name="Entertainment"
+                            categories={entertainmentLinks}
                         />
-                        <AiOutlineSearch className="absolute right-0 top-0 mr-4 mt-[0.75rem]" />
+                        <NewsLink name="Sport" categories={sportLinks} />
+                        <NewsLink
+                            name="Lifestyle"
+                            categories={lifestyleLinks}
+                        />
+                        <Link href="/our-team">
+                            <a className="cursor-pointer rounded-md border-2 border-gray-200 bg-gray-100 px-2 py-1 duration-150 hover:bg-gray-200 hover:text-gray-900">
+                                Our Team
+                            </a>
+                        </Link>
+                    </nav>
+                    <div className="mx-auto mt-4 flex items-center gap-4 xl:mt-0">
+                        <div className="relative mx-auto text-gray-600">
+                            <input
+                                className="h-10 rounded-lg border-2 border-gray-300 bg-white px-5 pr-16 text-sm focus:outline-none"
+                                type="search"
+                                name="search"
+                                placeholder="Search"
+                            />
+                            <AiOutlineSearch className="absolute right-0 top-0 mr-4 mt-[0.75rem]" />
+                        </div>
+                        {status === "loading" ? (
+                            <Spinner size="6" />
+                        ) : status === "unauthenticated" ? (
+                            <Button onClick={() => signIn("google")}>
+                                Login
+                            </Button>
+                        ) : (
+                            <UserDropdown />
+                        )}
                     </div>
-                    {status === "loading" ? (
-                        <Spinner size="6" />
-                    ) : status === "unauthenticated" ? (
-                        <Button onClick={() => signIn("google")}>Login</Button>
-                    ) : (
-                        <UserDropdown />
-                    )}
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </Bounce>
     );
 };
 
--- src/components/NavBar/NewsLink.tsx
+++ src/components/NavBar/NewsLink.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { AiOutlineArrowDown, AiOutlineArrowUp } from "react-icons/ai";
-import { Menu } from "@headlessui/react";
+import { Menu, Transition } from "@headlessui/react";
+
 import { Category } from "../../lib/categories";
 
 import NextLink from "../../ui/NextLink";
@@ -13,7 +14,7 @@ interface Props {
 
 const NewsLink: React.FC<Props> = ({ name, categories }) => {
     return (
-        <Menu as="div" className="relative inline-flex">
+        <Menu as="div" className="relative inline-flex select-none">
             <Menu.Button
                 as="div"
                 className="flex cursor-pointer items-center gap-1 rounded-md border-2 border-gray-200 bg-gray-100 px-2 py-1 duration-150 hover:bg-gray-200 hover:text-gray-900"
@@ -29,21 +30,31 @@ const NewsLink: React.FC<Props> = ({ name, categories }) => {
                     </>
                 )}
             </Menu.Button>
-            <Menu.Items className="absolute z-10 mt-12 w-36 rounded-md border border-gray-200 bg-gray-50 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                {categories.map((news, index) => (
-                    <Menu.Item
-                        as={NextLink}
-                        href={
-                            news.url
-                                ? news.url
-                                : `/${name.toLowerCase()}?filter=${news.id}`
-                        }
-                        key={index}
-                    >
-                        <DropdownElement>{news.name}</DropdownElement>
-                    </Menu.Item>
-                ))}
-            </Menu.Items>
+            <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+            >
+                <Menu.Items className="absolute mt-12 w-36 rounded-md border border-gray-200 bg-gray-50 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    {categories.map((news, index) => (
+                        <Menu.Item
+                            as={NextLink}
+                            href={
+                                news.url
+                                    ? news.url
+                                    : `/${name.toLowerCase()}?filter=${news.id}`
+                            }
+                            key={index}
+                        >
+                            <DropdownElement>{news.name}</DropdownElement>
+                        </Menu.Item>
+                    ))}
+                </Menu.Items>
+            </Transition>
         </Menu>
     );
 };
