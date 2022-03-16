0f13032970974122c1f7871f63279bb1f6ef8ee9
David Ilie <david@davidilie.com>
2022-03-16 12:37:08 +0100

    api data is fetched

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import { GetServerSideProps } from "next";
 import { NextSeo } from "next-seo";
 import { VscChevronLeft, VscChevronRight } from "react-icons/vsc";
 import { Slide } from "react-awesome-reveal";
+import { formatDistance } from "date-fns";
+import { useShortcut } from "litkey";
 
 import prisma from "../lib/prisma";
 import { Article } from "../types/Article";
@@ -12,48 +14,60 @@ interface Props {
 }
 
 const Home: React.FC<Props> = ({ featuredPosts }) => {
-    const [selectedIndex, setSelectedIndex] = useState<number>(0);
+    const [index, setSelectedIndex] = useState<number>(0);
+
+    useShortcut("left", () => index !== 0 && setSelectedIndex(index - 1));
+    useShortcut(
+        "right",
+        () => index !== featuredPosts.length - 1 && setSelectedIndex(index + 1)
+    );
 
     return (
         <>
             <NextSeo title="Home" />
             <div className="flex min-h-screen flex-grow items-center">
-                <div className="-ml-24 mt-20 flex w-full justify-center">
+                <div className="-ml-20 mt-20 flex w-full justify-center gap-24">
                     <Slide cascade triggerOnce direction="left">
-                        <div className="relative flex items-center gap-4">
+                        <div className="relative -mr-96 flex items-center gap-4">
                             <button
                                 className="cursor-pointer rounded-full bg-gray-100 p-4 duration-150 hover:bg-gray-200 focus:ring-2 focus:ring-gray-500 disabled:cursor-not-allowed disabled:text-gray-400 hover:disabled:bg-gray-100"
-                                disabled={selectedIndex === 0}
-                                onClick={() =>
-                                    setSelectedIndex(selectedIndex - 1)
-                                }
+                                disabled={index === 0}
+                                onClick={() => setSelectedIndex(index - 1)}
+                                title="Back"
                             >
                                 <VscChevronLeft size={30} />
                             </button>
-                            <div className="w-[70%] duration-150 hover:shadow-xl">
-                                <div className="h-26 absolute bottom-0 w-[70%] rounded-br-md rounded-bl-md bg-gray-900 bg-opacity-70 py-2 px-4 text-lg text-white">
+                            <div className="w-[50%] duration-150 hover:shadow-xl">
+                                <h1 className="absolute top-0 left-0 ml-20 rounded-t-md bg-gray-900 bg-opacity-70 py-2 px-4 text-lg font-medium text-white">
+                                    {featuredPosts[index].title} -{" "}
+                                    <span className="font-normal italic text-gray-300">
+                                        {formatDistance(
+                                            new Date(
+                                                featuredPosts[index].createdAt
+                                            ),
+                                            new Date(),
+                                            { addSuffix: true }
+                                        )}
+                                    </span>
+                                </h1>
+                                <div className="h-26 absolute bottom-0 w-[50%] rounded-b-md bg-gray-900 bg-opacity-70 py-2 px-4 text-lg text-white">
                                     <h1 className="line-clamp-3">
                                         <span className="text-2xl font-bold">
                                             BREAKING NEWS:
                                         </span>{" "}
-                                        Your mum is on fire, but the house
-                                        aswell because she forgo💀 to switch off
-                                        the oven
+                                        {featuredPosts[index].description}
                                     </h1>
                                 </div>
                                 <img
-                                    src="/kings-alicante-1.jpg"
-                                    className="aspect-[14/9] rounded-md object-cover"
+                                    src={featuredPosts[index].cover}
+                                    className="aspect-[3/2] rounded-md object-cover"
                                 />
                             </div>
                             <button
                                 className="cursor-pointer rounded-full bg-gray-100 p-4 duration-150 hover:bg-gray-200 focus:ring-2 focus:ring-gray-500 disabled:cursor-not-allowed disabled:text-gray-400 hover:disabled:bg-gray-100"
-                                disabled={
-                                    selectedIndex === featuredPosts.length
-                                }
-                                onClick={() =>
-                                    setSelectedIndex(selectedIndex + 1)
-                                }
+                                disabled={index === featuredPosts.length - 1}
+                                onClick={() => setSelectedIndex(index + 1)}
+                                title="Next"
                             >
                                 <VscChevronRight size={30} />
                             </button>
