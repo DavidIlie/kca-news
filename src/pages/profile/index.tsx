8cfa110ac2ed028e380bbca8b2a9d63156be61c2
David Ilie <david@davidilie.com>
2022-03-21 14:39:37 +0100

    fix a bug the bitchy way

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -7,7 +7,9 @@ const PersonalProfileViewer: React.FC = () => {
     return (
         <>
             <DefaultSeo title="Profile" />
-            <div className="mb-20 flex min-h-screen flex-grow px-4 sm:pt-24 lg:px-0"></div>
+            <div className="mb-20 flex min-h-screen flex-grow px-4 sm:pt-24 lg:px-0">
+                
+            </div>
         </>
     );
 }
