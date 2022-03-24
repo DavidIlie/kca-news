a5fa928c1a043e4c3a468595c3427f9076b823c6
David Ilie <david@davidilie.com>
2022-03-24 21:14:53 +0100

    fix page

--- src/pages/dashboard/writer/edit/[id].tsx
+++ src/pages/dashboard/writer/edit/[id].tsx
@@ -5,12 +5,14 @@ import { getSession } from "next-auth/react";
 
 import prisma from "../../../../lib/prisma";
 import { Article } from "../../../../types/Article";
+import { User } from "../../../../types/User";
 
 interface Props {
+   user: User;
    article: Article;
 }
 
-const ArticleEditor: React.FC<Props> = ({ article }) => {
+const ArticleEditor: React.FC<Props> = ({ user, article }) => {
    return (
       <>
          <DefaultSeo title={article.title} />
@@ -26,6 +28,7 @@ export const getServerSideProps: GetServerSideProps = async ({
    query,
    req,
 }) => {
+   const { id } = query;
    const session = await getSession({ req });
 
    if (!session || session?.user?.isAdmin ? false : !session?.user?.isWriter)
@@ -36,7 +39,30 @@ export const getServerSideProps: GetServerSideProps = async ({
          },
       };
 
-   const article = await prisma.article.findFirst({ where: {} });
+   const article = session?.user?.isAdmin
+      ? await prisma.article.findFirst({
+           where: {
+              id: id as string,
+           },
+        })
+      : await prisma.article.findFirst({
+           where: { id: id as string, user: session?.user?.id },
+        });
+
+   if (!article)
+      return {
+         redirect: {
+            destination: "/dashboard/writer",
+            permanent: false,
+         },
+      };
+
+   return {
+      props: {
+         user: session?.user,
+         article: JSON.parse(JSON.stringify(article)),
+      },
+   };
 };
 
 export default ArticleEditor;
