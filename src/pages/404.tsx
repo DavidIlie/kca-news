aefc829cc42b82f8706c42fd1d4fcb632b76fe07
David Ilie <david@davidilie.com>
2022-04-03 18:56:37 +0200

    fix next auth

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -8,9 +8,11 @@ const NotFoundPage: React.FC = () => {
    return (
       <>
          <DefaultSeo title="Not Found" />
-         <div className="my-24 flex flex-grow items-center justify-center px-4 pt-20">
+         <div className="my-24 flex flex-grow items-center justify-center px-4 py-24 sm:pt-48 lg:px-0">
             <div className="text-center">
-               <h1 className="mb-4 text-4xl font-semibold">Not Found</h1>
+               <h1 className="mb-4 text-4xl font-semibold text-red-500 sm:text-6xl">
+                  Not Found
+               </h1>
                <Link href="/">
                   <Button className="mx-auto">Go Home</Button>
                </Link>
