57e0f14cf09f8a62f3009d45f236b421acd1994a
David Ilie <david@davidilie.com>
2022-04-06 20:20:41 +0200

    fix dockerfile

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -13,21 +13,23 @@ const Footer: React.FC = () => {
       <footer className="borderColor border-t-2 dark:bg-dark-bg">
          <div className="container mx-auto flex flex-col flex-wrap pb-12 pt-6 md:flex-row md:flex-nowrap md:items-center lg:items-start">
             <div className="mx-auto w-64 flex-shrink-0 text-center sm:pl-14 md:mx-0 md:text-left">
-               <a className="flex items-center justify-center font-medium text-gray-900 dark:text-gray-100 md:justify-start">
-                  <svg
-                     xmlns="http://www.w3.org/2000/svg"
-                     fill="none"
-                     stroke="currentColor"
-                     strokeLinecap="round"
-                     strokeLinejoin="round"
-                     strokeWidth="2"
-                     className="h-10 w-10 rounded-full bg-blue-500 p-2 text-white dark:text-black"
-                     viewBox="0 0 24 24"
-                  >
-                     <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                  </svg>
-                  <span className="ml-3 text-xl">KCA News</span>
-               </a>
+               <Link href={process.env.NEXT_PUBLIC_APP_URL}>
+                  <a className="flex items-center justify-center font-medium text-gray-900 dark:text-gray-100 md:justify-start">
+                     <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="h-10 w-10 rounded-full bg-blue-500 p-2 text-white dark:text-black"
+                        viewBox="0 0 24 24"
+                     >
+                        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                     </svg>
+                     <span className="ml-3 text-xl">KCA News</span>
+                  </a>
+               </Link>
                <p className="mt-2 text-sm text-gray-500 dark:text-gray-200">
                   Make the school cool.
                </p>
@@ -38,7 +40,7 @@ const Footer: React.FC = () => {
                   .map((location, index) => (
                      <div key={index} className="px-8">
                         <Link href={`/${location}`}>
-                           <a className="mb-3 text-sm font-medium tracking-widest text-gray-900 duration-150 hover:text-blue-500 dark:text-gray-100 dark:hover:text-blue-500">
+                           <a className="mb-3 text-base font-medium tracking-widest text-gray-900 duration-150 hover:text-blue-500 dark:text-gray-100 dark:hover:text-blue-500">
                               {getFormmatedLocation(location).toUpperCase()}
                            </a>
                         </Link>
