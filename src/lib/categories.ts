2580e3db484f9390604e6a6cf72f87fd515b3f23
David Ilie <david@davidilie.com>
2022-05-11 16:28:01 +0200

    added categories

--- src/lib/categories.ts
+++ src/lib/categories.ts
@@ -4,16 +4,16 @@ export type Locations =
    | "sports"
    | "society"
    | "science-environment"
-   | "fashion"
-   | "entertainment";
+   | "entertainment"
+   | "arts";
 
 export const visibleLocations: Locations[] = ["news", "humanities", "sports"];
 
 export const moreLocations: Locations[] = [
    "society",
    "science-environment",
-   "fashion",
    "entertainment",
+   "arts",
 ];
 
 export const fullLocations = visibleLocations.concat(moreLocations);
@@ -37,16 +37,91 @@ export const links: Category[] = [
    {
       id: "our-school",
       name: "Our School",
-      location: visibleLocations.concat(moreLocations),
+      location: fullLocations,
+   },
+   {
+      id: "latest-news",
+      name: "Latest News",
+      location: ["news"],
+   },
+   {
+      id: "global",
+      name: "Global",
+      location: ["news"],
    },
    {
       id: "world",
-      name: "World",
-      location: visibleLocations.concat(moreLocations),
+      name: "Worldwide",
+      location: fullLocations.filter((s) => s !== "news"),
    },
    {
       id: "politics",
       name: "Politics",
-      location: ["news"],
+      location: ["humanities"],
+   },
+   {
+      id: "history",
+      name: "History",
+      location: ["humanities"],
+   },
+   {
+      id: "economics",
+      name: "Economics",
+      location: ["humanities"],
+   },
+   {
+      id: "travel",
+      name: "Travel",
+      location: ["society"],
+   },
+   {
+      id: "well-being",
+      name: "Well-Being",
+      location: ["society"],
+   },
+   {
+      id: "food",
+      name: "Food",
+      location: ["society"],
+   },
+   {
+      id: "culture",
+      name: "Culture",
+      location: ["society"],
+   },
+   {
+      id: "celebrities",
+      name: "Celebrities",
+      location: ["society"],
+   },
+   {
+      id: "gaming",
+      name: "Gaming",
+      location: ["entertainment"],
+   },
+   {
+      id: "cinema",
+      name: "Cinema",
+      location: ["entertainment"],
+   },
+   {
+      id: "books",
+      name: "Books",
+      location: ["entertainment"],
+   },
+   {
+      id: "music",
+      name: "Music",
+      location: ["arts"],
+   },
+   {
+      id: "dance",
+      name: "Dance",
+      location: ["arts"],
+   },
+   {
+      id: "art",
+      name: "General Art",
+      location: ["arts"],
    },
 ];
