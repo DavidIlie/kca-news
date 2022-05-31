4903cbb3fcdcc9c4829e5364bc7dbf8218a8b000
David Alexandru Ilie <david@davidilie.com>
2022-05-31 09:09:17 +0200

    Update comment.ts

--- src/schema/comment.ts
+++ src/schema/comment.ts
@@ -1,5 +1,5 @@
 import * as yup from "yup";
 
 export const crudCommentSchema = yup.object().shape({
-   message: yup.string().min(2).max(100).required(),
+   message: yup.string().min(2).max(500).required(),
 });
