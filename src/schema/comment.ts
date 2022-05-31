dac44e91576bb85848a5fbe986b91dc975c7dfa6
David Ilie <david@davidilie.com>
2022-05-31 09:30:08 +0200

    buenas

--- src/schema/comment.ts
+++ src/schema/comment.ts
@@ -1,5 +1,9 @@
 import * as yup from "yup";
 
 export const crudCommentSchema = yup.object().shape({
-   message: yup.string().min(2).max(500).required(),
+   message: yup
+      .string()
+      .min(2, "Your message cannot be less than 2 characters!")
+      .max(500, "Your message cannot be more than 500 characters!")
+      .required(),
 });
