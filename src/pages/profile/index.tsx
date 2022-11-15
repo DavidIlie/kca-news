3dcf0c52bc23dd0b8e39c4fdc57d1e09fcd928f6
Tim <65774333+timthedev07@users.noreply.github.com>
2022-11-15 21:07:54 +0000

    include co-articles in user's own profile

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -7,77 +7,89 @@ import { User } from "@/types/User";
 import ProfileViewer from "@/components/ProfileViewer";
 
 interface Props {
-   user: User;
+  user: User;
 }
 
 const PersonalProfileViewer: React.FC<Props> = ({ user }) => {
-   return (
-      <div className="flex items-center justify-center flex-grow px-4 my-12 sm:pt-20 lg:px-0">
-         <ProfileViewer user={user} editable={true} />
-      </div>
-   );
+  return (
+    <div className="my-12 flex flex-grow items-center justify-center px-4 sm:pt-20 lg:px-0">
+      <ProfileViewer user={user} editable={true} />
+    </div>
+  );
 };
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-   const session = await getSession({ req });
+  const session = await getSession({ req });
 
-   if (!session)
-      return {
-         redirect: {
-            destination: "/api/auth/signin",
-            permanent: false,
-         },
-      };
-
-   const user = await prisma.user.findUnique({
-      where: { id: session.user!.id },
-      include: {
-         comments: {
-            where: {
-               article: {
-                  published: true,
-               },
-            },
-            include: {
-               user: true,
-               article: true,
-            },
-            orderBy: {
-               createdAt: "desc",
-            },
-            take: 1,
-         },
-         upvotes: true,
-         downvotes: true,
-         articles: {
-            include: {
-               writer: true,
-            },
-            where: { published: true },
-            orderBy: {
-               upvotes: {
-                  _count: "desc",
-               },
-            },
-            take: 3,
-         },
-         _count: {
-            select: {
-               articles: true,
-               coArticles: true,
-               comments: true,
-               upvotes: true,
-               downvotes: true,
-            },
-         },
+  if (!session)
+    return {
+      redirect: {
+        destination: "/api/auth/signin",
+        permanent: false,
       },
-   });
+    };
 
-   return {
-      props: {
-         user: JSON.parse(JSON.stringify(user)),
+  const user = await prisma.user.findUnique({
+    where: { id: session.user!.id },
+    include: {
+      comments: {
+        where: {
+          article: {
+            published: true,
+          },
+        },
+        include: {
+          user: true,
+          article: true,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+        take: 1,
       },
-   };
+      upvotes: true,
+      downvotes: true,
+      articles: {
+        include: {
+          writer: true,
+        },
+        where: { published: true },
+        orderBy: {
+          upvotes: {
+            _count: "desc",
+          },
+        },
+        take: 3,
+      },
+      coArticles: {
+        include: {
+          writer: true,
+        },
+        where: { published: true },
+        orderBy: {
+          upvotes: {
+            _count: "desc",
+          },
+        },
+        take: 3,
+      },
+      _count: {
+        select: {
+          articles: true,
+          coArticles: true,
+          comments: true,
+          upvotes: true,
+          downvotes: true,
+        },
+      },
+    },
+  });
+
+  return {
+    props: {
+      user: JSON.parse(JSON.stringify(user)),
+    },
+  };
 };
 
 export default PersonalProfileViewer;
