45770cc900c6e37bc99f7d044ea1a7c806dfe3df
David Ilie <david@davidilie.com>
2022-12-06 15:24:55 +0000

    fix detonation switch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import type { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { NextSeo } from "next-seo";
