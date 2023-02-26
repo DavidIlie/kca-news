3751f0321ccd2c20526b3fd0c26d23a3025ea954
Tim <65774333+timthedev07@users.noreply.github.com>
2023-02-26 19:49:21 +0000

    error handling for debug

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,26 +1,30 @@
 import React from "react";
 import { DefaultSeo } from "next-seo";
-
 import Link from "next/link";
-
 import { Button } from "@/ui/Button";
+import { ErrorProps } from "next/error";
 
-const Error: React.FC = () => {
-   return (
-      <>
-         <DefaultSeo title="Error" />
-         <div className="flex items-center justify-center flex-grow px-4 my-24 sm:pt-16 lg:px-0">
-            <div className="text-center">
-               <h1 className="mb-4 text-4xl font-semibold text-red-500 sm:text-6xl">
-                  Error
-               </h1>
-               <Link href="/">
-                  <Button className="mx-auto">Go Home</Button>
-               </Link>
-            </div>
-         </div>
-      </>
-   );
+const Error: React.FC<ErrorProps> = ({ statusCode, title }) => {
+  return (
+    <>
+      <DefaultSeo title="Error" />
+      <div className="my-24 flex flex-grow items-center justify-center px-4 sm:pt-16 lg:px-0">
+        <div className="text-center">
+          <h1 className="mb-4 text-4xl font-semibold text-red-500 sm:text-6xl">
+            {statusCode} Error
+          </h1>
+          {title && (
+            <h3 className="mb-5 text-2xl text-red-500/80 sm:text-3xl">
+              {title}
+            </h3>
+          )}
+          <Link href="/">
+            <Button className="mx-auto">Go Home</Button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Error;
